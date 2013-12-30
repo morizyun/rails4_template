654cc0dba08f5dd8ca2d25c7d1dc30bf65ddc434
morizyun <merii.ken@gmail.com>
2013-12-30 11:02:04 +0900

    fix someting

--- app_template.rb
+++ app_template.rb
@@ -137,14 +137,14 @@ run "echo 'web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb' > Procfile"
 
 ## Errbit ###################################################
 if yes?('Use Errbit? [yes or ENTER]')
-  run 'wget https://raw.github.com/morizyun/rails4_template/master/config/initilizers/errbit.rb -P config/initilizers'
+  run 'wget https://raw.github.com/morizyun/rails4_template/master/config/initializers/errbit.rb -P config/initializers'
   run 'Register app to Errbit/Airbrake'
   host_name = ask('hostname[host name or errbit.herokuapp.com]')
   host_name = 'http://errbit.herokuapp.com/' if host_name !~ /^http/
   open host_name
   key_value = ask('key value?')
-  gsub_file 'config/initilizers/errbit.rb', /%HOST_NAME/, host_name.gsub(/(https?:\/\/|\/$)/)
-  gsub_file 'config/initilizers/errbit.rb', /%KEY_VALUE/, key_value
+  gsub_file 'config/initializers/errbit.rb', /%HOST_NAME/, host_name.gsub(/(https?:\/\/|\/$)/)
+  gsub_file 'config/initializers/errbit.rb', /%KEY_VALUE/, key_value
 end
 
 ## MongoDB ###################################################
