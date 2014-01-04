1985d131ee68e6ba627f94d4c4a7338622160315
morizyun <merii.ken@gmail.com>
2014-01-04 11:13:35 +0900

    fix something

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,11 @@
 namespace = [Rails.application.class.parent_name, Rails.env].join ':'
 if Rails.env.production?
-  redis_uri = URI(ENV['REDISCLOUD_URL'])
-  Redis.current = Redis.new(host: redis_uri.host, port: redis_uri.port, password: redis_uri.password)
-  Redis.current = Redis::Namespace.new(namespace, Redis.new(host: redis_uri.host, port: redis_uri.port, password: redis_uri.password))
+  # herokuの初期 asset compileでENVがうまく読み込まれていないっぽいので対策
+  if ENV['REDISCLOUD_URL']
+    redis_uri = URI(ENV['REDISCLOUD_URL'])
+    Redis.current = Redis.new(host: redis_uri.host, port: redis_uri.port, password: redis_uri.password)
+    Redis.current = Redis::Namespace.new(namespace, Redis.new(host: redis_uri.host, port: redis_uri.port, password: redis_uri.password))
+  end
 else
   Redis.current = Redis::Namespace.new(namespace, Redis.new(host: '127.0.0.1', port: 6379))
 end
